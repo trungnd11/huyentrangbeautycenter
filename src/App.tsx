0583f896c3197f6d75f49a2f33ee25aeb9171953
trungnd9 <96941235+trungnd9@users.noreply.github.com>
2022-09-02 20:29:07 +0700

    Fix login Fb

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
-
-import { useEffect, useState } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Customer from "./layout/Customer";
 import Login from "./pages/login/Login";
 
 function App() {
-  const location = useLocation();
-  const { key } = location;
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    window.scrollTo({
-      top: 0,
-    });
-
-    const timeLoading = setTimeout(() => {
-      setIsLoading(false);
-    }, 1500);
-
-    return () => {
-      clearTimeout(timeLoading);
-    }
-  }, [key]);
-
   return (
     <div className="App">
       <Routes>
